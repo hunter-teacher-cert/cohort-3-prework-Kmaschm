5e5177977c93cd2e5aa74611955300235836edaa
Kmaschm <96199973+Kmaschm@users.noreply.github.com>
2021-12-27 23:43:20 +0000

    Updated Time.java with variables as ints instead of doubles after rereading Ch 2.

--- pre02/Time.java
+++ pre02/Time.java
@@ -4,36 +4,36 @@ import java.util.*;
 public class Time {
   public static void main(String[] args)
 {
-    double hour = 18.0;
-    double minute = 29.0;
-    double second = 33.0;
+    int hour = 18;
+    int minute = 29;
+    int second = 33;
 
-    double secInMin = 60.0;
-    double secInHour = 60.0*secInMin;
-    double secInDay = 24.0*secInHour;
+    int secInMin = 60;
+    int secInHour = 60*secInMin;
+    int secInDay = 24*secInHour;
 
     // calculate & display number of seconds since midnight
-    double secSinceMid = second + minute*secInMin + hour*secInHour;
+    int secSinceMid = second + minute*secInMin + hour*secInHour;
 
     System.out.println("Seconds since midnight: " + secSinceMid);
 
     // calculate and display the number of seconds remaining in the day
-    double secLeftInDay = secInDay - secSinceMid;
+    int secLeftInDay = secInDay - secSinceMid;
 
     System.out.println("Number of seconds left in day: " + secLeftInDay);
 
     // calculate and display the percentage of the day that has passed
-    double perOfDayPassed = (secSinceMid / secInDay) * 100;
+    int perOfDayPassed = (secSinceMid * 100) / secInDay;
 
     System.out.println("Percentage of day that has passed: " + perOfDayPassed + "%");
 
     // elapsed time since starting this exercise
     // original values: hour: 18, minute: 4, second: 10
-    double prevHour = 18.0;
-    double prevMin = 4.0;
-    double prevSec = 10.0;
+    int prevHour = 18;
+    int prevMin = 4;
+    int prevSec = 10;
 
-    double elapsedTime = (hour - prevHour)*secInHour + (minute - prevMin)*secInMin + (second - prevSec);
+    int elapsedTime = (hour - prevHour)*secInHour + (minute - prevMin)*secInMin + (second - prevSec);
 
     System.out.println("Elapsed time since start of exercise: " + elapsedTime);
 
