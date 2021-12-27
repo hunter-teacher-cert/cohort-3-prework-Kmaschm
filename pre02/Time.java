668d88ae1706949491282ba36adf8c505e88d02f
Kmaschm <96199973+Kmaschm@users.noreply.github.com>
2021-12-27 23:52:49 +0000

    Updated Time.java in for Assignment 2 to use literals

--- pre02/Time.java
+++ pre02/Time.java
@@ -4,26 +4,26 @@ import java.util.*;
 public class Time {
   public static void main(String[] args)
 {
+    final int SEC_IN_MIN = 60;
+    final int SEC_IN_HOUR = 60*SEC_IN_MIN;
+    final int SEC_IN_DAY = 24*SEC_IN_HOUR;
+
     int hour = 18;
     int minute = 29;
     int second = 33;
 
-    int secInMin = 60;
-    int secInHour = 60*secInMin;
-    int secInDay = 24*secInHour;
-
     // calculate & display number of seconds since midnight
-    int secSinceMid = second + minute*secInMin + hour*secInHour;
+    int secSinceMid = second + minute*SEC_IN_MIN + hour*SEC_IN_HOUR;
 
     System.out.println("Seconds since midnight: " + secSinceMid);
 
     // calculate and display the number of seconds remaining in the day
-    int secLeftInDay = secInDay - secSinceMid;
+    int secLeftInDay = SEC_IN_DAY - secSinceMid;
 
     System.out.println("Number of seconds left in day: " + secLeftInDay);
 
     // calculate and display the percentage of the day that has passed
-    int perOfDayPassed = (secSinceMid * 100) / secInDay;
+    int perOfDayPassed = (secSinceMid * 100) / SEC_IN_DAY;
 
     System.out.println("Percentage of day that has passed: " + perOfDayPassed + "%");
 
@@ -33,7 +33,7 @@ public class Time {
     int prevMin = 4;
     int prevSec = 10;
 
-    int elapsedTime = (hour - prevHour)*secInHour + (minute - prevMin)*secInMin + (second - prevSec);
+    int elapsedTime = (hour - prevHour)*SEC_IN_HOUR + (minute - prevMin)*SEC_IN_DAY + (second - prevSec);
 
     System.out.println("Elapsed time since start of exercise: " + elapsedTime);
 
