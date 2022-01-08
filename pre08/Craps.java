789d7e41b181604c028b984da56ff3d295815cae
Kmaschm <96199973+Kmaschm@users.noreply.github.com>
2022-01-08 19:51:06 +0000

    Setup Craps.java in /pre08 for final project: + Class is created with main() + Comments about specifications including rules and required methods

---
+++ pre08/Craps.java
@@ -0,0 +1,36 @@
+
+public class Craps {
+
+/*
+* Simulates a game of Craps:
+*
+* Main program must take in number of rounds, then play number of rounds specified, and output status of rounds as they occur
+* 
+*
+* roll: takes an integer and returns a random number between 1 and the integer (inclusive)
+*
+* shoot: takes in the number of dice and max value for one die, returns result of rolling the dice 
+*
+* round: takes in no parameters, returns if round is won or lost
+*
+* Rules for a round:
+* 1. Shooter rolls/shoots dice.
+*
+* If shooter rolls:
+* 2, 3, 12  --> Craps, shooter loses
+* 7, 11 --> Natural shooter wins
+* Any other number not specified:
+* value is now called Point
+* shooter must roll until they roll Point (wins) or 7 (loses)
+*
+* 
+*  
+*/
+
+public static void main(String[] args) {
+  
+}
+
+
+
+}
